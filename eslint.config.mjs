ec677580edf2ee79098ae1711526c2d3aa3eb789
hyojin <hy9jin4@gmail.com>
2026-03-17 10:07:45 +0900

    chore: clsx, tailwind-merge, cva, Storybook 라이브러리 설치

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -5,46 +8,38 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import unusedImports from "eslint-plugin-unused-imports";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  eslintConfigPrettier,
-
-  {
-    plugins: {
-      "unused-imports": unusedImports,
-      "simple-import-sort": simpleImportSort,
-    },
-    rules: {
-      // unused import 제거
-      "@typescript-eslint/no-unused-vars": "off",
-      "unused-imports/no-unused-imports": "error",
-
-      // unused vars 처리
-      "unused-imports/no-unused-vars": [
-        "warn",
-        {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
-        },
-      ],
-
-      // import 정렬
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-    },
+const eslintConfig = defineConfig([...nextVitals, ...nextTs, eslintConfigPrettier, {
+  plugins: {
+    "unused-imports": unusedImports,
+    "simple-import-sort": simpleImportSort,
   },
+  rules: {
+    // unused import 제거
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
 
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+    // unused vars 처리
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+
+    // import 정렬
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+}, // Override default ignores of eslint-config-next.
+globalIgnores([
+  // Default ignores of eslint-config-next:
+  ".next/**",
+  "out/**",
+  "build/**",
+  "next-env.d.ts",
+]), ...storybook.configs["flat/recommended"]]);
 
 export default eslintConfig;
