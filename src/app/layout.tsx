cd24011d6deae09ebe533560143963a7dbc5e150
daeya0406 <daeya0406@gmail.com>
2026-03-23 01:27:29 +0900

    feat: toast (임의 디자인으로 임시적용)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
+import { ToastProvider } from "@/components/common/Toast";
 import { pretendard } from "@/lib/fonts";
 import { initMocks } from "@/mocks";
 import MSWInitializer from "@/mocks/MSWInitializer";
@@ -22,7 +23,14 @@ export default function RootLayout({
     >
       <body className={pretendard.className}>
         <MSWInitializer>
-          <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
+          <ReactQueryClientProvider>
+            <ToastProvider
+              position="bottom-right"
+              max={5}
+            >
+              {children}
+            </ToastProvider>
+          </ReactQueryClientProvider>
         </MSWInitializer>
         <Analytics />
         <SpeedInsights />
