736c947b1d7f960125f5b4f47fe79638e24656f9
hyojin <hy9jin4@gmail.com>
2026-03-23 23:35:28 +0900

    page 이전 상태로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { Dropdown } from "../hooks/useDropdown/Dropdown";
-
 export default function Home() {
-  return (
-    <Dropdown
-      options={["todo", "doing", "done"]}
-      selected="todo"
-      className="w-40"
-    />
-  );
+  return <h1 className="text-background-elevated-normal-alternative">home</h1>;
 }
