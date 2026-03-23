feae04b19119c4f1bdd454d07da58ce21df08a10
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-03-23 18:55:59 +0900

    feat(#47): iconMap과 Icon 컴포넌트 구현

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
   turbopack: {
     rules: {
       "*.svg": {
-        loaders: ["@svgr/webpack"],
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+            },
+          },
+        ],
         as: "*.js",
       },
     },
