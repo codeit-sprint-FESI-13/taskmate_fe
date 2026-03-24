eb9ecc42d15ca9195377931bbc11a39528e08e39
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-03-24 18:03:10 +0900

    feat(#56): 복잡한 파일 폴더로 정리

--- src/components/todo/TodoItem/index.tsx
+++ src/components/todo/TodoItem/index.tsx
@@ -1,8 +1,8 @@
-import { Day } from "./Day";
-import { Goal } from "./Goal";
-import { Name } from "./Name";
+import { Day } from "./Day/Day";
+import { Goal } from "./Goal/Goal";
+import { Name } from "./Name/Name";
 import { Row } from "./Row";
-import { Team } from "./Team";
+import { Team } from "./Team/Team";
 import { Wrapper } from "./Wrapper";
 
 export const TodoItem = {
