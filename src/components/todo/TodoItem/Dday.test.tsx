9b2badb84b50914cf02301557dd5e85401db02f1
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-03-24 11:50:10 +0900

    feat(#56): Name 컴포넌트 테스트 AAA 패턴으로 변경

--- src/components/todo/TodoItem/Dday.test.tsx
+++ src/components/todo/TodoItem/Dday.test.tsx
@@ -1,16 +1,10 @@
 import { render, screen } from "@testing-library/react";
 
-import { Name } from "./Name";
+import { Dday } from "./Dday";
 
 describe("TodoItem/Name 컴포넌트 테스트", () => {
   test("Name 컴포넌트에 값이 제대로 렌더링 되는지 테스트", () => {
-    // Arrange
-    const name = "Name 컴포넌트 테스트";
-
-    // Act
-    render(<Name>{name}</Name>);
-
-    // Assert
+    render(<Dday>Name 컴포넌트 테스트</Dday>);
     expect(screen.getByText("Name 컴포넌트 테스트")).toBeInTheDocument();
   });
 });
