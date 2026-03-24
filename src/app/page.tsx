6f0dc11090f3ea180208a4766e5c3a05631e6580
hyojin <hy9jin4@gmail.com>
2026-03-24 14:45:55 +0900

    feat:TextButton 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,3 @@
-import Button from "@/components/common/Button/Button";
-
 export default function Home() {
-  return (
-    <section className="flex flex-col gap-4">
-      <h2 className="text-lg font-bold text-gray-800">Buttons</h2>
-      <div className="flex items-center gap-4">
-        <Button
-          variant="primary"
-          size="md"
-        >
-          Primary Button
-        </Button>
-
-        <Button
-          isDisabled
-          size="md"
-        >
-          Disabled Button
-        </Button>
-      </div>
-    </section>
-  );
+  return <h1 className="text-background-elevated-normal-alternative">home</h1>;
 }
