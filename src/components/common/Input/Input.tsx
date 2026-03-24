ebea40c6250bc201b27bc2f23b3dc216d776c3f8
LeeJaein <devxoox1106@gmail.com>
2026-03-24 13:31:22 +0900

    refactor(#53): Input rightIcon props 외부 제어 방식으로 변경

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,11 +1,26 @@
 "use client";
 import { cva, VariantProps } from "class-variance-authority";
-import { InputHTMLAttributes, useState } from "react";
+import { InputHTMLAttributes } from "react";
 
-import ClearIcon from "@/components/common/Icons/ClearIcon";
-import EyeOffIcon from "@/components/common/Icons/EyeOffIcon";
-import EyeOnIcon from "@/components/common/Icons/EyeOnIcon";
 import { cn } from "@/utils/utils";
+
+/**
+ * @example
+ * // clear 버튼
+ * const [value, setValue] = useState("");
+ *
+ * <Input
+ *   value={value}
+ *   onChange={(e) => setValue(e.target.value)}
+ *   rightIcon={
+ *     value && (
+ *       <button onClick={() => setValue("")}>
+ *         <ClearIcon />
+ *       </button>
+ *     )
+ *   }
+ * />
+ */
 
 const InputVariants = cva(
   "w-full h-11 pt-3 pr-3 pb-3 pl-4 gap-1 rounded-xl text-sm leading-5 text-label-normal font-medium border border-gray-300 placeholder:text-label-alternative lg:text-base lg:leading-6 lg:font-medium lg:h-14 lg:rounded-2xl focus:outline-none focus:border-blue-800 hover:border-blue-300",
@@ -31,28 +46,22 @@ interface InputProps
     VariantProps<typeof InputVariants> {
   errorMessage?: string;
   supportingText?: string;
-  onClear?: () => void;
+  rightIcon?: React.ReactNode;
 }
 
 const Input = ({
-  type,
   variant,
   value,
   className,
   errorMessage,
   supportingText,
-  onClear,
+  rightIcon,
   ...props
 }: InputProps) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const toggleShow = () => {
-    setShowPassword((prev) => !prev);
-  };
   return (
     <div className="flex flex-col gap-2.5">
       <div className="relative">
         <input
-          type={type === "password" && showPassword ? "text" : type}
           className={cn(
             InputVariants({
               variant: errorMessage ? "error" : variant,
@@ -65,32 +74,7 @@ const Input = ({
           {...props}
         ></input>
         <div className="absolute top-1/2 right-3 flex -translate-y-1/2 items-center gap-2">
-          {/* type="password"일 때 눈 아이콘 */}
-          {type === "password" && variant !== "disabled" && (
-            <button
-              type="button"
-              onClick={toggleShow}
-            >
-              {showPassword ? (
-                <EyeOnIcon className="text-gray-300" />
-              ) : (
-                <EyeOffIcon className="text-gray-300" />
-              )}
-            </button>
-          )}
-
-          {/* 값이 있을 때 X 버튼 */}
-          {value &&
-            variant !== "disabled" &&
-            type !== "password" &&
-            onClear && (
-              <button
-                type="button"
-                onClick={onClear}
-              >
-                <ClearIcon className="text-gray-300" />
-              </button>
-            )}
+          {rightIcon}
         </div>
       </div>
       {(errorMessage || supportingText) && (
