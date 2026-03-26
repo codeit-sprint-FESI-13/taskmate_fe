876c6a2e3a38ce765d62acc71fbf89592e756347
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-03-26 17:48:23 +0900

    feat(#73): 팀 생성 Form에 api 연결

--- src/components/team/createForm/Form.tsx
+++ src/components/team/createForm/Form.tsx
@@ -1,16 +1,51 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 import Button from "@/components/common/Button/Button";
 import Input from "@/components/common/Input";
+import { TEAM_NAME_MAX_LENGTH } from "@/constants/team";
+import { teamApi } from "@/features/team/api";
+import type { ApiError } from "@/lib/api/types";
 
 export default function Form() {
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const name = formData.get("name") as string;
+
+    try {
+      const response = await teamApi.create(name);
+
+      if (response.success) {
+        router.back();
+      }
+    } catch (error) {
+      const apiError = error as ApiError;
+
+      setErrorMessage(apiError.message);
+    }
+  };
+
   return (
-    <form className="flex flex-col items-start justify-start gap-8">
+    <form
+      className="flex flex-col items-start justify-start gap-8"
+      onSubmit={handleSubmit}
+    >
       <label className="typography-heading-2 font-semibold">팀 이름</label>
 
       <Input
+        name="name"
+        required
+        type="text"
+        maxLength={TEAM_NAME_MAX_LENGTH}
         className="mobile:w-[303px] tablet:w-[496px]"
         placeholder="팀 이름을 입력해주세요"
+        errorMessage={errorMessage ?? undefined}
       />
 
       <Button
