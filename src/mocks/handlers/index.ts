aa1bb78d787610875a1bc57bf638a8540dc9d470
LeeJaein <devxoox1106@gmail.com>
2026-03-29 22:56:09 +0900

    chore(#83): 회원가입/이메일 중복확인 MSW 핸들러 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,4 +1,5 @@
+import { authHandlers } from "./auth";
 import { postsHandlers } from "./posts";
 import { teamsHandlers } from "./teams";
 
-export const handlers = [...postsHandlers, ...teamsHandlers];
+export const handlers = [...postsHandlers, ...teamsHandlers, ...authHandlers];
