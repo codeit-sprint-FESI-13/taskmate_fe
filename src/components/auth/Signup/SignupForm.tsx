ed5a48eaf3d7d6895ba7a1df786085bd191a7993
LeeJaein <devxoox1106@gmail.com>
2026-03-29 23:00:46 +0900

    feat(#83): 회원가입 폼에 API 연결 및 이메일 중복 확인 구현

--- src/components/auth/Signup/SignupForm.tsx
+++ src/components/auth/Signup/SignupForm.tsx
@@ -19,6 +19,7 @@ const SignupForm = () => {
     handleSubmit,
     handleChange,
     handleBlur,
+    handleEmailDuplicate,
   } = useSignupForm();
   useOAuthError("signup");
   return (
@@ -45,12 +46,21 @@ const SignupForm = () => {
           />
         </div>
         <div className="flex flex-col gap-1.5">
-          <label
-            htmlFor="email"
-            className="typography-label-1 font-semibold"
-          >
-            이메일
-          </label>
+          <div className="flex justify-between">
+            <label
+              htmlFor="email"
+              className="typography-label-1 font-semibold"
+            >
+              이메일
+            </label>
+            <button
+              type="button"
+              onClick={handleEmailDuplicate}
+            >
+              이메일중복체크
+            </button>
+          </div>
+
           <Input
             id="email"
             name="email"
