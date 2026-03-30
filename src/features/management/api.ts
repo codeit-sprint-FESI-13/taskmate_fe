55c473914732e387d9c11711ad1ebbe4a94611e6
hyojin <hy9jin4@gmail.com>
2026-03-30 23:32:26 +0900

    feat: 팀명 조회/수정 api 연결

--- src/features/management/api.ts
+++ src/features/management/api.ts
@@ -1,11 +1,22 @@
 import { apiClient } from "@/lib/api/client";
 
-import type { ResponseInviteSuccess } from "./types";
+import type { TeamNameResponseSuccess } from "./types";
+import type { InviteResponseSuccess } from "./types";
 
+// 팀 상세 정보 요청
+export const teamDetailData = {
+  read: (teamId: number) =>
+    apiClient.get<TeamNameResponseSuccess>(`/teams/${teamId}`, {}),
+  create: (teamId: number, name: string) =>
+    apiClient.patch<TeamNameResponseSuccess>(`/teams/${teamId}`, {
+      name,
+    }),
+};
+
+// 팀 초대 요청
 export const inviteApi = {
-  // 팀 초대 요청
-  create: (teamId: string, email: string) =>
-    apiClient.post<ResponseInviteSuccess>(`/teams/${teamId}/invitations`, {
+  create: (teamId: number, email: string) =>
+    apiClient.post<InviteResponseSuccess>(`/teams/${teamId}/invitations`, {
       email,
     }),
 };
