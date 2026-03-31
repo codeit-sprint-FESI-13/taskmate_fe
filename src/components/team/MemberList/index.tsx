411eba79177ae2fda3d0dd4ac1bbc8b4a05359b4
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-04-01 02:42:56 +0900

    feat(#131): 팀내 멤버 리스트 TODO 정리

--- src/components/team/MemberList/index.tsx
+++ src/components/team/MemberList/index.tsx
@@ -5,8 +5,8 @@ import TextButton from "@/components/common/TextButton/TextButton";
 import { useTeamId } from "@/features/team/hooks/useTeamId";
 import { useTeamLeaveModal } from "@/features/team/hooks/useTeamLeaveModal";
 
-// @TODO: GET memberList API 및 useSuspenseInfiniteQuery 적용
-// @TODO: 목표 목록 조회 시 무한 스크롤 처리
+// @TODO: GET memberList API 적용 ( 효진님 작업 이후 반영 => 중복 작업 )
+// @TODO: 목표 목록 조회 시 무한 스크롤 처리 (useSuspenseInfiniteQuery)
 export const MemberList = () => {
   const teamId = useTeamId();
   const { openLeaveTeamModal } = useTeamLeaveModal(teamId);
