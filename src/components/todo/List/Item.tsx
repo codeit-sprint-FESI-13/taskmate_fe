f48f5bef62cfe5e3ecf52ebab010e77e4fbec119
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-04-01 14:55:13 +0900

    feat(#132): 할일 상세보기 데이터 연결

--- src/components/todo/List/Item.tsx
+++ src/components/todo/List/Item.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { useSuspenseQuery } from "@tanstack/react-query";
+
 import { Icon } from "@/components/common/Icon";
+import { useGoalId } from "@/features/goal/hooks/useGoalId";
+import { goalQueries } from "@/features/goal/query/goal.queryKey";
+import { useTeamId } from "@/features/team/hooks/useTeamId";
+import { teamQueries } from "@/features/team/query/team.queryKey";
 import { useTodoDeleteModal } from "@/features/todo/hooks/useTodoDeleteModal";
 import { useTodoDetailModal } from "@/features/todo/hooks/useTodoDetailModal";
 import { Todo } from "@/features/todo/types";
@@ -14,8 +20,22 @@ interface ItemProps {
 }
 
 export const Item = ({ todo }: ItemProps) => {
+  const goalId = useGoalId();
+  const teamId = useTeamId();
+  const {
+    data: { goalName },
+  } = useSuspenseQuery(goalQueries.getSummary(goalId));
+
+  const {
+    data: { teamName },
+  } = useSuspenseQuery(teamQueries.summary(teamId));
+
   const { openTodoDeleteModal } = useTodoDeleteModal();
-  const { openTodoDetailModal } = useTodoDetailModal();
+  const { openTodoDetailModal } = useTodoDetailModal({
+    todo,
+    goalName,
+    teamName,
+  });
 
   return (
     <li
@@ -25,6 +45,7 @@ export const Item = ({ todo }: ItemProps) => {
       <div className="flex min-w-0 flex-1 items-center justify-start gap-2">
         <TodoStatusSelect todo={todo} />
         <TodoItem.Name>{todo.title}</TodoItem.Name>
+        {/* @TODO: DDay  */}
         <TodoItem.Day color="gray">D-5</TodoItem.Day>
       </div>
       <div className="flex items-center justify-end gap-3">
