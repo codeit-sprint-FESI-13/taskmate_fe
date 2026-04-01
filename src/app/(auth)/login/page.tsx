946aa3b074725eafaf2b8090bd5ca2ce39b49ad9
LeeJaein <devxoox1106@gmail.com>
2026-04-01 14:53:26 +0900

    fix: login 페이지 useSearchParams Suspense로 감싸기

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import LoginForm from "@/components/auth/Login/LoginForm";
 import SnsLoginButtons from "@/components/auth/SnsLoginButtons";
 
 const LoginPage = () => {
   return (
-    <>
+    // useSearchParams() 사용으로 인한 CSR Suspense 추가
+    // TODO: 공통 스켈레톤 UI 컴포넌트 구현 후 fallback으로 교체
+    <Suspense fallback={null}>
       <LoginForm />
       <SnsLoginButtons
         label="SNS 계정으로 로그인"
         type="login"
       />
-    </>
+    </Suspense>
   );
 };
 
