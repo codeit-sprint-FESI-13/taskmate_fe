1d7a06776d7f910a9247333d34086662278dcc37
JongJin Kim <tlfvm04@naver.com>
2026-04-01 13:42:50 +0900

    hotfix: merge 간 발생한 오류 수정

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -3,5 +3,5 @@ import { postsHandlers } from "./posts";
 import { teamsHandlers } from "./teams";
 import { todosHandlers } from "./todos";
 
-export const handlers = [...postsHandlers, ...teamsHandlers, ...todosHandlers, ...authHandlers]];
+export const handlers = [...postsHandlers, ...teamsHandlers, ...todosHandlers, ...authHandlers];
 
