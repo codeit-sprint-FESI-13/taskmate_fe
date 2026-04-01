6796d521c3120bf1e58147f784778119eaf8a4d3
hyojin <hy9jin4@gmail.com>
2026-04-01 09:38:51 +0900

    feat: 오름차순 로직 추가

--- src/utils/formatMemberList.ts
+++ src/utils/formatMemberList.ts
@@ -1,10 +1,13 @@
 type Member = {
+  id: number;
   userId: number;
 };
 
 export function formatMemberList(members: Member[], userId: number): Member[] {
   const currentUser = members.find((member) => member.userId === userId);
-  const otherMembers = members.filter((member) => member.userId !== userId);
+  const otherMembers = members
+    .filter((member) => member.userId !== userId)
+    .sort((a, b) => a.id - b.id);
 
   return currentUser ? [currentUser, ...otherMembers] : otherMembers;
 }
