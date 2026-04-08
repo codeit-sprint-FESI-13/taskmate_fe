f8fbde7567a183bce60386b82a64d826d84beefc
hyojin <hy9jin4@gmail.com>
2026-04-08 10:46:42 +0900

    fix: ProfileCard 컴포넌트 수정

--- src/components/management/ProfileCard.tsx
+++ src/components/management/ProfileCard.tsx
@@ -1,170 +1,54 @@
-import { cva } from "class-variance-authority";
-
 import defaultAvatar from "@/assets/images/avatar.png";
-import Button from "@/components/common/Button/Button";
 import Crown from "@/components/common/Icons/Crown";
-import RightArrow from "@/components/common/Icons/RightArrow";
-import { memberRoleApi } from "@/features/management/api";
-import { memberApi } from "@/features/management/api";
-import { MemberRole } from "@/features/management/types";
-import Dropdown from "@/hooks/useDropdown/Dropdown";
-import { cn } from "@/utils/utils";
-
-const profileCardVariants = cva(
-  "self-start flex h-[45px] cursor-pointer justify-between w-full",
-  {
-    variants: {
-      variant: {
-        gnb: "pl-3 pr-[20px] border border-gray-300 h-[64px] rounded-full",
-        "gnb-sm":
-          "pl-3 pr-[20px] border border-gray-300 h-[64px] rounded-full py-4",
-        team: "",
-        "team-sm": "",
-        admin: "",
-        "admin-sm": "",
-      },
-    },
-  },
-);
 
 type ProfileCardProps = {
-  id: number;
-  teamId: number;
-  avatar: string;
-  isAdmin?: boolean;
-  nickName: string;
-  isMe?: boolean;
+  avatar?: string;
+  hasCrownIcon?: boolean;
+  name: string;
+  tag?: string;
   email: string;
-  onDeleteMember?: () => void;
-  variant?:
-    | "default"
-    | "gnb"
-    | "gnb-sm"
-    | "team"
-    | "team-sm"
-    | "admin"
-    | "admin-sm";
 };
 
 const ProfileCard = ({
-  id,
-  teamId,
   avatar,
-  isAdmin = false,
-  nickName,
-  isMe = false,
+  hasCrownIcon = false,
+  tag,
+  name,
   email,
-  variant = "default",
-  onDeleteMember,
 }: ProfileCardProps) => {
-  const avatarSrc = avatar?.trim() ? avatar : defaultAvatar.src;
-  const isGnb = variant === "gnb" || variant === "gnb-sm";
-  const selectedRole = isAdmin ? "어드민" : "팀원";
-
-  const updateRole = async (value: typeof selectedRole) => {
-    let role: MemberRole = "MEMBER";
-
-    if (value === "어드민") {
-      role = "ADMIN";
-    } else if (value === "팀원") {
-      role = "MEMBER";
-    } else {
-      return;
-    }
-
-    await memberRoleApi.update(teamId, id, role);
-  };
-
-  const deleteMember = async (memberId: number): Promise<void> => {
-    try {
-      await memberApi.delete(teamId, memberId);
-    } catch (error) {
-      console.error("delete member error", error);
-    }
-  };
+  const avatarSrc = avatar?.trim() ? avatar : defaultAvatar.src; // 기본값 처리
 
   return (
-    <div
-      className={cn(
-        profileCardVariants({
-          variant: variant === "default" ? undefined : variant,
-        }),
-      )}
-    >
-      <div className="inline-flex h-11.25 cursor-pointer items-center gap-2 self-start">
-        {/* avatar */}
-        <div className="relative flex h-10 w-10 justify-center">
-          <img
-            src={avatarSrc}
-            alt="Avatar Image"
-            className="h-10 w-10 shrink-0 rounded-full object-cover"
-          />
-          {isAdmin && (
-            <span className="absolute -top-0.75 -right-0.75 h-4 w-4">
-              <Crown />
+    <div className="flex w-fit items-center gap-2">
+      {/* avatar */}
+      <div className="relative h-10 w-10">
+        <img
+          src={avatarSrc}
+          alt="Avatar Image"
+          className="h-10 w-10 shrink-0 rounded-full object-cover"
+        />
+        {hasCrownIcon && (
+          <span className="absolute -top-0.75 -right-0.75 h-4 w-4">
+            <Crown />
+          </span>
+        )}
+      </div>
+
+      {/* info */}
+      <div className="flex flex-col">
+        <div className="inline-flex items-center">
+          {/* NickName */}
+          <span className="text-sm">{name}</span>
+          {tag && (
+            <span className="ml-0.75 rounded-lg bg-gray-100 px-2.5 py-1 text-[12px] text-gray-400">
+              {tag}
             </span>
           )}
         </div>
 
-        {/* info */}
-        <div className={cn("flex flex-col")}>
-          {/* NickName Wapper */}
-          <div className="inline-flex items-center">
-            {/* NickName */}
-            <span className="text-sm">{nickName}</span>
-            {(variant === "team" ||
-              variant === "team-sm" ||
-              variant === "admin" ||
-              variant === "admin-sm") &&
-              isMe && (
-                <span className="ml-0.75 rounded-lg bg-gray-100 px-2.5 py-1 text-[12px] text-gray-400">
-                  나
-                </span>
-              )}
-
-            {/* Gnb Icon */}
-            {isGnb && (
-              <span className="flex h-4 w-4 items-center justify-center">
-                <RightArrow className="text-gray-400" />
-              </span>
-            )}
-          </div>
-
-          {/* Email */}
-          {variant !== "gnb-sm" && email && (
-            <span className="text-[13px] text-gray-400">{email}</span>
-          )}
-        </div>
+        {/* Email */}
+        <span className="text-[13px] text-gray-400">{email}</span>
       </div>
-      {/* Admin Button */}
-      {(variant === "admin" || variant === "admin-sm") && (
-        <div
-          className={cn(
-            "",
-            variant === "admin" && "flex items-center gap-1.5",
-            variant === "admin-sm" && "",
-          )}
-        >
-          {/* 권한 선택 */}
-          <div className="flex items-center self-center">
-            <Dropdown
-              options={["어드민", "팀원"]}
-              selected={selectedRole}
-              onSelect={updateRole}
-            />
-          </div>
-
-          {/* 팀원 삭제 버튼 */}
-          <Button
-            onClick={onDeleteMember}
-            variant="secondary"
-            size="sm"
-            className="rounded-lg text-gray-500 ring-gray-200 hover:ring-gray-300"
-          >
-            팀원 삭제
-          </Button>
-        </div>
-      )}
     </div>
   );
 };
