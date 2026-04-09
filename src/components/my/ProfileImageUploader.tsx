6f4a5e64643056ddc3575a057734f06a3d68d0e8
LeeJaein <devxoox1106@gmail.com>
2026-04-09 09:50:23 +0900

    feat(#177): 파일 input 연결 및 이미지 업로드 트리거 구현

--- src/components/my/ProfileImageUploader.tsx
+++ src/components/my/ProfileImageUploader.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 
 import ActionButton from "../common/ActionButton/ActionButton";
 import UserAvatar from "../common/UserAvatar/UserAvatar";
 
 const ProfileImageUploader = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleClick = () => {
+    inputRef.current?.click();
+  };
+  const handleChange = () => {};
+
   return (
     <div className="relative flex w-fit">
       <UserAvatar
@@ -13,6 +20,12 @@ const ProfileImageUploader = () => {
       <ActionButton
         action="edit"
         className="absolute right-0 bottom-0"
+        onClick={handleClick}
+      />
+      <input
+        ref={inputRef}
+        type="file"
+        className="hidden"
       />
     </div>
   );
