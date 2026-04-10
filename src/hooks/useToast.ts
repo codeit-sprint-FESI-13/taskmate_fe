8f5e3b811288b3bb8ec80649141d24b822fbb1c9
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-04-10 14:12:43 +0900

    feat(#200): Toast 디자인 수정 및 연관 불필요 코드 제거

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { ToastContext } from "@/components/common/Toast";
+import { ToastContext } from "@/components/common/Toast/ToastProvider";
 
 export function useToast() {
   const context = React.useContext(ToastContext);
--- src/providers/ToastProvider.tsx
+++ src/providers/ToastProvider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 
-import { ToastProvider } from "@/components/common/Toast";
+import { ToastProvider } from "@/components/common/Toast/ToastProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ToastProvider position="top-right">{children}</ToastProvider>;
