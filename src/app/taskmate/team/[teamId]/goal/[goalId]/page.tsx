e2cee8c729da5ee5679179715ea3f74874972ead
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-04-10 15:41:02 +0900

    feat(#180): 목표 페이지 상단, 환영메시지 구현

--- src/app/taskmate/team/[teamId]/goal/[goalId]/page.tsx
+++ src/app/taskmate/team/[teamId]/goal/[goalId]/page.tsx
@@ -1,25 +1,32 @@
-import { Spacing } from "@/components/common/Spacing";
-import { Summary } from "@/components/goal/Summary";
-import { TodoSection } from "@/components/team/TodoSection";
+"use client";
+
+import AsyncBoundary from "@/components/common/AsyncBoundary";
+import Spinner from "@/components/common/Spinner";
+import { Heading } from "@/components/goal/Heading";
 
 export default function Page() {
   return (
-    <>
-      {/* @TODO: 사용자 정보(닉네임) 데이터와 연결하기 ( 재인님 설정 )*/}
-      <div className="flex flex-col items-start gap-[6px] px-2">
-        <h1 className="typography-title-2 text-gray-400">두잉두잉님</h1>
-        <span className="typography-title-2 text-label-neutral">
-          목표와 할 일을 확인해보세요!
-        </span>
-      </div>
+    <div className="flex w-full flex-col justify-start px-6 py-8">
+      <AsyncBoundary
+        loadingFallback={<Spinner size={60} />}
+        errorFallback={(error, onReset) => (
+          <Heading.Error
+            error={error}
+            onReset={onReset}
+          />
+        )}
+      >
+        <Heading />
+      </AsyncBoundary>
 
+      {/* 
       <Spacing size={64} />
 
       <Summary />
 
       <Spacing size={56} />
 
-      <TodoSection />
-    </>
+      <TodoSection /> */}
+    </div>
   );
 }
---
+++ src/components/goal/Heading/index.tsx
@@ -0,0 +1,4 @@
+import Error from "./Error";
+import HeadingComponent from "./Heading";
+
+export const Heading = Object.assign(HeadingComponent, { Error });
