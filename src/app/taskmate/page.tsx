3a51bc3865cabcf4433573ae32d81aa1f844ceaa
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-04-13 15:47:07 +0900

    hotfix: layout.tsx 제거 테스트

--- src/app/taskmate/page.tsx
+++ src/app/taskmate/page.tsx
@@ -4,37 +4,46 @@ import { Spacing } from "@/components/common/Spacing";
 import ProgressSection from "@/components/home/ProgressSection";
 import { TeamGoalsHorizontalSection } from "@/components/home/TeamGoalsHorizontalSection";
 import WelcomeBanner from "@/components/home/WelcomeBanner";
+import { NavigationBar } from "@/components/NavigationBar";
+import NavigationBarProvider from "@/components/NavigationBar/provider";
 
 export default function TaskmatePage() {
   return (
-    <div className="flex w-full max-w-full min-w-0 flex-col items-center justify-center">
-      <AsyncBoundary errorFallback={<div>error</div>}>
-        <WelcomeBanner />
-      </AsyncBoundary>
+    <NavigationBarProvider>
+      <div className="flex">
+        <NavigationBar />
+        <div className="mx-auto w-full px-[40px] py-[80px]">
+          <div className="flex w-full max-w-full min-w-0 flex-col items-center justify-center">
+            <AsyncBoundary errorFallback={<div>error</div>}>
+              <WelcomeBanner />
+            </AsyncBoundary>
 
-      <Spacing size={64} />
+            <Spacing size={64} />
 
-      <AsyncBoundary errorFallback={<div>error</div>}>
-        <ProgressSection />
-      </AsyncBoundary>
+            <AsyncBoundary errorFallback={<div>error</div>}>
+              <ProgressSection />
+            </AsyncBoundary>
 
-      <Spacing size={56} />
+            <Spacing size={56} />
 
-      <div className="flex w-full max-w-full min-w-0 flex-col gap-5">
-        <div className="flex w-full items-center gap-3">
-          <Icon
-            name="FlagGreen"
-            size={40}
-          />
-          <h2 className="typography-body-1 font-medium text-black">
-            팀별 진행 상황
-          </h2>
+            <div className="flex w-full max-w-full min-w-0 flex-col gap-5">
+              <div className="flex w-full items-center gap-3">
+                <Icon
+                  name="FlagGreen"
+                  size={40}
+                />
+                <h2 className="typography-body-1 font-medium text-black">
+                  팀별 진행 상황
+                </h2>
+              </div>
+
+              <AsyncBoundary errorFallback={<div>error</div>}>
+                <TeamGoalsHorizontalSection />
+              </AsyncBoundary>
+            </div>
+          </div>
         </div>
-
-        <AsyncBoundary errorFallback={<div>error</div>}>
-          <TeamGoalsHorizontalSection />
-        </AsyncBoundary>
       </div>
-    </div>
+    </NavigationBarProvider>
   );
 }
