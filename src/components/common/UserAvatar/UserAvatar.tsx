8821b2d23bae203b70b752a45f327fb73f5fca3e
LeeJaein <devxoox1106@gmail.com>
2026-04-13 13:20:33 +0900

    refactor(UserAvatar): className 병합을 cn() 유틸로 교체

--- src/components/common/UserAvatar/UserAvatar.tsx
+++ src/components/common/UserAvatar/UserAvatar.tsx
@@ -6,6 +6,7 @@ import avatar1 from "@/assets/images/avatar-blue-default.svg";
 import avatar2 from "@/assets/images/avatar-blue-glasses.svg";
 import avatar3 from "@/assets/images/avatar-green-default.svg";
 import avatar4 from "@/assets/images/avatar-green-glasses.svg";
+import { cn } from "@/utils/utils";
 
 const DEFAULT_AVATARS = [avatar1, avatar2, avatar3, avatar4];
 
@@ -20,7 +21,7 @@ const UserAvatar = ({ imageUrl, className }: UserAvatarProps) => {
   );
 
   return (
-    <div className={`relative ${className}`}>
+    <div className={cn("relative", className)}>
       {imageUrl ? (
         <Image
           src={imageUrl}
