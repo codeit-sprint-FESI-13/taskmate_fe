beb916d57e87ad666b856028d632a1c312b74a03
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-04-14 13:11:06 +0900

    feat(#207): todo list 요청 params 생성 함수 분리

--- src/features/todo/api.ts
+++ src/features/todo/api.ts
@@ -8,62 +8,47 @@ import type {
   UpdateTodoInput,
 } from "./types.ts";
 
+function todoListSearchParams(params: TodoListQueryParams) {
+  const optional = Object.fromEntries(
+    (
+      [
+        ["cursorDueDate", params.cursorDueDate],
+        ["cursorCreatedAt", params.cursorCreatedAt],
+        ["cursorId", params.cursorId],
+        ["limit", params.limit],
+      ] as const
+    ).filter(([, value]) => {
+      if (value === undefined) return false;
+      if (typeof value === "string" && value === "") return false;
+      return true;
+    }),
+  );
+
+  return {
+    sort: params.sort,
+    mineOnly: params.mineOnly ? "true" : "false",
+    titleContains: params.titleContains,
+    ...optional,
+  };
+}
+
 export const todoApi = {
   create: (goalId: string, todoData: CreateTodoInput) =>
     apiClient.post<ResponseCreateTodo>(`/api/goals/${goalId}/todos`, todoData),
 
   getTodoList: (goalId: string, params: TodoListQueryParams) =>
     apiClient.get<TodoListResponse>(`/api/goals/${goalId}/todos`, {
-      params: {
-        status: "TODO",
-        sort: params.sort,
-        mineOnly: params.mineOnly ? "true" : "false",
-        titleContains: params.titleContains,
-        ...(params.cursorDueDate
-          ? { cursorDueDate: params.cursorDueDate }
-          : {}),
-        ...(params.cursorCreatedAt
-          ? { cursorCreatedAt: params.cursorCreatedAt }
-          : {}),
-        ...(params.cursorId !== undefined ? { cursorId: params.cursorId } : {}),
-        ...(params.limit !== undefined ? { limit: params.limit } : {}),
-      },
+      params: { status: "TODO", ...todoListSearchParams(params) },
     }),
 
   getDoingList: (goalId: string, params: TodoListQueryParams) =>
     apiClient.get<TodoListResponse>(`/api/goals/${goalId}/todos`, {
-      params: {
-        status: "DOING",
-        sort: params.sort,
-        mineOnly: params.mineOnly ? "true" : "false",
-        titleContains: params.titleContains,
-        ...(params.cursorDueDate
-          ? { cursorDueDate: params.cursorDueDate }
-          : {}),
-        ...(params.cursorCreatedAt
-          ? { cursorCreatedAt: params.cursorCreatedAt }
-          : {}),
-        ...(params.cursorId !== undefined ? { cursorId: params.cursorId } : {}),
-        ...(params.limit !== undefined ? { limit: params.limit } : {}),
-      },
+      params: { status: "DOING", ...todoListSearchParams(params) },
     }),
 
   getDoneList: (goalId: string, params: TodoListQueryParams) =>
     apiClient.get<TodoListResponse>(`/api/goals/${goalId}/todos`, {
-      params: {
-        status: "DONE",
-        sort: params.sort,
-        mineOnly: params.mineOnly ? "true" : "false",
-        titleContains: params.titleContains,
-        ...(params.cursorDueDate
-          ? { cursorDueDate: params.cursorDueDate }
-          : {}),
-        ...(params.cursorCreatedAt
-          ? { cursorCreatedAt: params.cursorCreatedAt }
-          : {}),
-        ...(params.cursorId !== undefined ? { cursorId: params.cursorId } : {}),
-        ...(params.limit !== undefined ? { limit: params.limit } : {}),
-      },
+      params: { status: "DONE", ...todoListSearchParams(params) },
     }),
 
   patch: (goalId: string, todoId: string, todoData: UpdateTodoInput) => {
