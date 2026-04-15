4c56cbcfaa03a181c8b0db1847a0d69d8ef3a397
JongJin Kim <tlfvm04@naver.com>
2026-04-15 16:45:32 +0900

    hotfix: PR merge 오류 부분 해결

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-import { ToastProvider } from "@/components/common/Toast";
+import ToastProvider from "@/components/common/Toast";
 import NotificationSubscriber from "@/features/notification/NotificationSubscriber";
 import Overlay from "@/hooks/useOverlay/Overlay";
 import { pretendard } from "@/lib/fonts";
