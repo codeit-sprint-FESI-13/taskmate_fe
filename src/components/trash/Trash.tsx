1cf9a990d11fc9c52bf621149df315a519a89356
LeeJaein <devxoox1106@gmail.com>
2026-04-16 13:38:19 +0900

    feat(#226): 휴지통 UI 실제 데이터 연결

--- src/components/trash/Trash.tsx
+++ src/components/trash/Trash.tsx
@@ -1,13 +1,21 @@
 "use client";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 
-import PersonalTrash from "./PersonalTrash";
-import TeamTrash from "./TeamTrash";
-import TeamTrashDropdown from "./TeamTrash/TeamTrashDropdown";
-import TrashTabs, { TrashTab } from "./TrashTabs";
+import PersonalTrash from "@/components/trash/PersonalTrash";
+import TeamTrash from "@/components/trash/TeamTrash";
+import TeamTrashDropdown from "@/components/trash/TeamTrash/TeamTrashDropdown";
+import TrashEmpty from "@/components/trash/TrashEmpty";
+import TrashTabs, { TrashTab } from "@/components/trash/TrashTabs";
+import { teamQueries } from "@/features/team/query/team.queryKey";
 
 function Trash() {
-  const [activeTab, setActiveTab] = useState<TrashTab>("team");
+  const [activeTab, setActiveTab] = useState<TrashTab>("personal");
+  const { data: teams } = useSuspenseQuery(teamQueries.all());
+  const [selectedTeamId, setSeletedTeamId] = useState<number | undefined>(
+    teams[0]?.teamId,
+  );
+
   return (
     <div className="mt-20 flex w-full flex-col">
       <div className="tablet:flex-row tablet:items-center tablet:justify-end flex flex-col">
@@ -17,11 +25,23 @@ function Trash() {
             onTabChange={setActiveTab}
           />
         </div>
-        {activeTab === "team" && <TeamTrashDropdown />}
+        {activeTab === "team" && selectedTeamId !== undefined && (
+          <TeamTrashDropdown
+            teams={teams}
+            selectedTeamId={selectedTeamId}
+            onSelect={setSeletedTeamId}
+          />
+        )}
       </div>
 
       <div className="w-full py-5">
-        {activeTab === "personal" ? <PersonalTrash /> : <TeamTrash />}
+        {activeTab === "personal" ? (
+          <PersonalTrash />
+        ) : selectedTeamId !== undefined ? (
+          <TeamTrash selectedTeamId={selectedTeamId} />
+        ) : (
+          <TrashEmpty />
+        )}
       </div>
     </div>
   );
