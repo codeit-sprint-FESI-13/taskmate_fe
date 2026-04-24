10095d864fb7b15a996faf49b7015bac5f3a56d0
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-04-24 20:14:39 +0900

    hotfix: todo list 응답시 assignees값이 없을때 처리

--- src/features/todo/ui/List/TodoAssigneeAvatars.tsx
+++ src/features/todo/ui/List/TodoAssigneeAvatars.tsx
@@ -16,7 +16,7 @@ export const TodoAssigneeAvatars = ({
     <div className="flex items-center">
       {/* @TODO: 담당자 이미지 받아서 처리 필요 ( 중간 이후 )*/}
       {/* @TODO: mouse hover 시 담당자 정보 표시? */}
-      {assignees.slice(0, 4).map((assignee, index) => (
+      {(assignees ?? []).slice(0, 4).map((assignee, index) => (
         <span
           key={assignee.userId}
           className="relative -ml-[14px] shrink-0 first:ml-0"
