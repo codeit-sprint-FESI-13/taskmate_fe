0e5c7e781663c73a6a2d6ecb1047e5b3f99140a3
hyojin <hy9jin4@gmail.com>
2026-05-01 09:03:43 +0900

    refactor(#248): REACT QUERY + MSW

--- src/features/notification/query/notificationInfiniteQueries.ts
+++
@@ -1,7 +0,0 @@
-import { NotificationApi } from "@/entities/notification";
-import { createPaginationOptions } from "@/shared/utils/createPaginationOptions";
-
-export const notificationInfiniteQueries = {
-  notificationsInfinite: () =>
-    createPaginationOptions("notifications", NotificationApi.get),
-};
