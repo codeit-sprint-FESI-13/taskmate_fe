bb93f6e7d83d9385bc34dd8b0a85c9ede182bdda
김종진(SugarSyrup) <tlfvm04@naver.com>
2026-05-03 22:27:03 +0900

    MSW 설정 변경

--- src/shared/mock/index.ts
+++ src/shared/mock/index.ts
@@ -1,6 +1,5 @@
 export async function initMocks() {
-  // 개발 환경이 아니라면 실행 X
-  if (process.env.NODE_ENV !== "development") return;
+  // if (process.env.NEXT_PUBLIC_USE_MSW !== "true") return;
   if (typeof window === "undefined") {
     // 서버 사이드
     const { server } = await import("./server");
